dda13200dde49cf2e52b5da73f6b358a17dde8ad
idrak sheikh <idraksheikh333@gmail.com>
2022-02-09 17:45:13 +0530

    updated

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default function App() {
-  return (
+  return (<>
+  <Image
+        
+        source={require('.\logo\HomeEstate-logo-2-.png')}
+      />
+     <Text style={styles.heading}>HomeEstate</Text>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
+      <Text>Please Enter Your Address</Text>
+      <TextInput placeholder='We will predict your house price'></TextInput>
       <StatusBar style="auto" />
-    </View>
+    </View></>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#90EE90',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading:{
+    flex: 0,
+    backgroundColor: '#fff',
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
